c2d7fbe3e2ff7bb40c0e5a5d48d2b8e530fbeabe
Jay Gohil <70004683+jay352@users.noreply.github.com>
2021-08-27 11:30:44 +0530

    Add files via upload

---
+++ database/migrations/2021_08_23_083531_create_task_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTaskTable extends Migration {
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up() {
+		Schema::create('task', function (Blueprint $table) {
+			$table->id();
+			$table->bigInteger('user_id')->unsigned();
+			$table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+			$table->string('task');
+			$table->boolean('status');
+			$table->timestamp('created_at');
+			$table->timestamp('updated_at');
+
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down() {
+		Schema::dropIfExists('task');
+	}
+}
